f1f6548d04687fa9a5c23627b40e4e8105c46444
Anya L. Keller <kelleranyal@gmail.com>
2021-02-16 18:15:14 -0500

    oh lorrd help

---
+++ test/index.js
@@ -0,0 +1,9 @@
+import assert from "assert";
+
+describe("Inanity", () => {
+  describe("ohmygod", () => {
+    it("It should always be true", () => {
+      assert.equal(true, true);
+    });
+  });
+});
