a898dbe8bcb13e25eb4c4d541d11ec86d189e832
Anya L. Keller <kelleranyal@gmail.com>
2021-02-16 16:32:35 -0500

    removed shabang

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-console */
-// shebang!!!
-#! /usr/bin/env node
+/* 
+OH GOD IDK WHAT I'M DOING
+
+good luck using this
+*/
 
 console.log("dotenvit has joined your party!");
 
 exports.insanity = function() {
   console.log("I'm not crazy...\n\n\n\n...Am I?");
-}
+};
