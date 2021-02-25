236d7aa94405666768ce9f4fb2759251cce85150
Anya L. Keller <kelleranyal@gmail.com>
2021-02-25 18:47:02 -0500

    added inquirer for no reason that I'm aware of

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,14 @@
 module.exports = {
   env: {
-    browser: true,
     commonjs: true,
     es2021: true,
-    node: true,
+    node: true
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-  },
-  plugins: ["react"],
+  extends: ["eslint:recommended"],
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
-    semi: ["error", "always"],
-  },
+    semi: ["error", "always"]
+  }
 };
--- src/dotenvit.js
+++ src/dotenvit.js
@@ -1,15 +1,36 @@
+const inquirer = require("inquirer");
 console.log("dotenvit has joined your party!");
+
+async function promptUser() {
+  const QUESTIONS = [
+    {
+      type: "input",
+      name: "inputThing",
+      message: "Type a thing"
+    },
+    {
+      type: "list",
+      name: "listThing",
+      message: "Choose a thing",
+      choices: ["option0", "option1", "option2"]
+    }
+  ];
+  let userAnswers = await inquirer.prompt(QUESTIONS);
+  return userAnswers;
+}
 
 function insanity() {
   console.log("I'm not crazy...\n\n\n\n...Am I?");
   return true;
 }
 
-function dotenvit(args) {
+async function dotenvit(args) {
+  let userAnswers = await promptUser();
   console.log("Your args:", args);
+  console.log("Your choices:", userAnswers);
 }
 
 module.exports = {
   insanity,
-  dotenvit,
+  dotenvit
 };
